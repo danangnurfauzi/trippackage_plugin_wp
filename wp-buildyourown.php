f63f4f8dfba0ae0591c6f09f20ad18179bc3b453
danangnurfauzi <danangnurfauzi@gmail.com>
2017-01-01 11:24:25 +0700

    mini tripackage modify

--- wp-buildyourown.php
+++ wp-buildyourown.php
@@ -441,13 +441,16 @@
                             'posts_per_page' => -1                         
 						);
 						$obj_optionsf = new WP_Query( $arr_options_args );?>
-					<select name="pack_face1" id="pack_face1">
-						<option value=""> Select Face </div>
+					<select name="pack_face1" id="pack_face1" class="price_face1">
+						<option data-price-face1="0" value="0"> Select Face </div>
 					  <?php 
 						$i=0;
                         foreach ( $obj_optionsf->posts as $obj_option ) :
+                        	
+                        	$_product = wc_get_product( $obj_option->ID );
+                        	
 						     $i++;	
-							echo '<option value="' . $obj_option->ID . '">'.$obj_option->post_title.'</option>';
+							echo '<option data-price-face1="'.$_product->get_regular_price().'" value="' . $obj_option->ID . '">'.$obj_option->post_title.'</option>';
 						endforeach;
 						       
 						?>
@@ -479,13 +482,16 @@
 						);
 						$obj_optionsb = new WP_Query( $arr_options_args );?>
 						
-					<select name="pack_band1" id="pack_band1">
-						<option value=""> Select Band </div>
+					<select name="pack_band1" id="pack_band1" class="price_band1">
+						<option data-price-band1="0" value="0" value=""> Select Band </div>
 					  <?php 
 						$i=0;
                         foreach ( $obj_optionsb->posts as $obj_option ) :
+                        	
+                        	$_product = wc_get_product( $obj_option->ID );
+                        	
 						     $i++;	
-							echo '<option value="' . $obj_option->ID . '">'.$obj_option->post_title.'</option>';
+							echo '<option data-price-band1="'.$_product->get_regular_price().'" value="' . $obj_option->ID . '">'.$obj_option->post_title.'</option>';
 						endforeach;
 						       
 						?>
@@ -501,13 +507,16 @@
 				<div class="faceHead">Choose a Watch Face 2</div>
 			</div>
               <div class="facescontent" id="page-1">
-					<select name="pack_face2" id="pack_face2">
-						<option value=""> Select Face </div>
+					<select name="pack_face2" id="pack_face2" class="price_face2">
+						<option data-price-face2="0" value="0"> Select Face </div>
 					  <?php 
 						$i=0;
                         foreach ( $obj_optionsf->posts as $obj_option ) :
+                        	
+                        	$_product = wc_get_product( $obj_option->ID );
+                        	
 						     $i++;	
-							echo '<option value="' . $obj_option->ID . '">'.$obj_option->post_title.'</option>';
+							echo '<option data-price-face2="'.$_product->get_regular_price().'" value="' . $obj_option->ID . '">'.$obj_option->post_title.'</option>';
 						endforeach;
 						       
 						?>
@@ -521,13 +530,16 @@
 			</div>
             <div class="facescontent">				
                 				
-					<select name="pack_band2" id="pack_band2">
-						<option value=""> Select Band </div>
+					<select name="pack_band2" id="pack_band2" class="price_band2">
+						<option data-price-band2="0" value="0"> Select Band </div>
 					  <?php 
 						$i=0;
                         foreach ( $obj_optionsb->posts as $obj_option ) :
+                        	
+                        	$_product = wc_get_product( $obj_option->ID );
+                        	
 						     $i++;	
-							echo '<option value="' . $obj_option->ID . '">'.$obj_option->post_title.'</option>';
+							echo '<option data-price-band2="'.$_product->get_regular_price().'" value="' . $obj_option->ID . '">'.$obj_option->post_title.'</option>';
 						endforeach;
 						       
 						?>
@@ -544,13 +556,16 @@
 			</div>
               <div class="facescontent" id="page-1">
                   
-					<select name="pack_face3" id="pack_face3">
-						<option value=""> Select Face </div>
+					<select name="pack_face3" id="pack_face3" class="price_face3">
+						<option data-price-face3="0" value="0"> Select Face </div>
 					  <?php 
 						$i=0;
                         foreach ( $obj_optionsf->posts as $obj_option ) :
+                        	
+                        	$_product = wc_get_product( $obj_option->ID );
+                        	
 						     $i++;	
-							echo '<option value="' . $obj_option->ID . '">'.$obj_option->post_title.'</option>';
+							echo '<option data-price-face3="'.$_product->get_regular_price().'" value="' . $obj_option->ID . '">'.$obj_option->post_title.'</option>';
 						endforeach;
 						       
 						?>
@@ -564,13 +579,16 @@
 			</div>
 				
               <div class="facescontent">	
-					<select name="pack_band3" id="pack_band3">
-						<option value=""> Select Band </div>
+					<select name="pack_band3" id="pack_band3" class="price_band3">
+						<option data-price-band3="0" value="0"> Select Band </div>
 					  <?php 
 						$i=0;
                         foreach ( $obj_optionsb->posts as $obj_option ) :
+                        	
+                        	$_product = wc_get_product( $obj_option->ID );
+                        	
 						     $i++;	
-							echo '<option value="' . $obj_option->ID . '">'.$obj_option->post_title.'</option>';
+							echo '<option data-price-band3="'.$_product->get_regular_price().'" value="' . $obj_option->ID . '">'.$obj_option->post_title.'</option>';
 						endforeach;
 						       
 						?>
@@ -583,8 +601,126 @@
 
 		<div class="addCart">
 				 <div class="info">
-		             <p id="totalPrice">Price = $<span id="total">90</span></p>
+		             <p id="totalPrice">Price = $<span id="total" class="akumulasi"></span></p>
 	             </div>
+
+<script type="text/javascript">
+	
+	var priceFace1 = jQuery('.price_face1').val();
+	var priceFace2 = jQuery('.price_face2').val();	
+	var priceFace3 = jQuery('.price_face3').val();
+	var priceBand1 = jQuery('.price_band1').val();
+	var priceBand2 = jQuery('.price_band2').val();
+	var priceBand3 = jQuery('.price_band3').val();
+	
+	var akumulasiTotal = Number(priceFace1) + Number(priceFace2) + Number(priceFace3) + Number(priceBand1) + Number(priceBand2) + Number(priceBand3);
+	
+	jQuery('.akumulasi').html(akumulasiTotal);	
+	//alert(priceFace1);
+	
+	jQuery('.price_face1').change(function(){
+	       var selected = jQuery(this).find('option:selected'); 
+	       var priceFace1 = selected.data('price-face1');
+	       
+	       var priceFace2 = jQuery('.price_face2').find('option:selected').data('price-face2');
+	       var priceFace3 = jQuery('.price_face3').find('option:selected').data('price-face3');
+	       
+	       var priceBand1 = jQuery('.price_band1').find('option:selected').data('price-band1');
+	       var priceBand2 = jQuery('.price_band2').find('option:selected').data('price-band2');
+	       var priceBand3= jQuery('.price_band3').find('option:selected').data('price-band3');
+	       
+	       var akumulasiTotal = Number(priceFace1) + Number(priceFace2) + Number(priceFace3) + Number(priceBand1) + Number(priceBand2) + Number(priceBand3);
+	
+		jQuery('.akumulasi').html(akumulasiTotal);
+	       
+	       //alert(akumulasiTotal);
+	});
+	
+	jQuery('.price_face2').change(function(){
+	       var selected = jQuery(this).find('option:selected');
+	       var priceFace2 = selected.data('price-face2');
+	       
+	       var priceFace1 = jQuery('.price_face1').find('option:selected').data('price-face1');
+	       var priceFace3 = jQuery('.price_face3').find('option:selected').data('price-face3');
+	       
+	       var priceBand1 = jQuery('.price_band1').find('option:selected').data('price-band1');
+	       var priceBand2 = jQuery('.price_band2').find('option:selected').data('price-band2');
+	       var priceBand3= jQuery('.price_band3').find('option:selected').data('price-band3');
+	       
+	       var akumulasiTotal = Number(priceFace1) + Number(priceFace2) + Number(priceFace3) + Number(priceBand1) + Number(priceBand2) + Number(priceBand3);
+	
+		jQuery('.akumulasi').html(akumulasiTotal);
+	});
+	
+	jQuery('.price_face3').change(function(){
+	       var selected = jQuery(this).find('option:selected');
+	       var priceFace3 = selected.data('price-face3');
+	       
+	       var priceFace2 = jQuery('.price_face2').find('option:selected').data('price-face2');
+	       var priceFace1 = jQuery('.price_face1').find('option:selected').data('price-face1');
+	       
+	       var priceBand1 = jQuery('.price_band1').find('option:selected').data('price-band1');
+	       var priceBand2 = jQuery('.price_band2').find('option:selected').data('price-band2');
+	       var priceBand3 = jQuery('.price_band3').find('option:selected').data('price-band3');
+	       
+	       var akumulasiTotal = Number(priceFace1) + Number(priceFace2) + Number(priceFace3) + Number(priceBand1) + Number(priceBand2) + Number(priceBand3);
+	
+		jQuery('.akumulasi').html(akumulasiTotal);
+	       
+	});
+	
+	jQuery('.price_band1').change(function(){
+	       var selected = jQuery(this).find('option:selected');	       
+	       var priceBand1 = selected.data('price-band1');
+	       
+	       var priceFace1 = jQuery('.price_face1').find('option:selected').data('price-face1');
+	       var priceFace2 = jQuery('.price_face2').find('option:selected').data('price-face2');
+	       var priceFace3 = jQuery('.price_face3').find('option:selected').data('price-face3');
+	       
+	       var priceBand2 = jQuery('.price_band2').find('option:selected').data('price-band2');
+	       var priceBand3 = jQuery('.price_band3').find('option:selected').data('price-band3');
+	       
+	       var akumulasiTotal = Number(priceFace1) + Number(priceFace2) + Number(priceFace3) + Number(priceBand1) + Number(priceBand2) + Number(priceBand3);
+	
+		jQuery('.akumulasi').html(akumulasiTotal);
+	       
+	});
+	
+	jQuery('.price_band2').change(function(){
+	       var selected = jQuery(this).find('option:selected');
+	       var priceBand2 = selected.data('price-band2');
+	       
+	       var priceFace1 = jQuery('.price_face1').find('option:selected').data('price-face1');
+	       var priceFace2 = jQuery('.price_face2').find('option:selected').data('price-face2');
+	       var priceFace3 = jQuery('.price_face3').find('option:selected').data('price-face3');
+	       
+	       var priceBand1 = jQuery('.price_band1').find('option:selected').data('price-band1');
+	       var priceBand3 = jQuery('.price_band3').find('option:selected').data('price-band3');
+	       
+	       var akumulasiTotal = Number(priceFace1) + Number(priceFace2) + Number(priceFace3) + Number(priceBand1) + Number(priceBand2) + Number(priceBand3);
+	
+		jQuery('.akumulasi').html(akumulasiTotal);
+	       
+	});
+	
+	jQuery('.price_band3').change(function(){
+	       var selected = jQuery(this).find('option:selected');
+	       var priceBand3 = selected.data('price-band3');
+	       
+	       var priceFace1 = jQuery('.price_face1').find('option:selected').data('price-face1');
+	       var priceFace2 = jQuery('.price_face2').find('option:selected').data('price-face2');
+	       var priceFace3 = jQuery('.price_face3').find('option:selected').data('price-face3');
+	       
+	       var priceBand2 = jQuery('.price_band2').find('option:selected').data('price-band2');
+	       var priceBand1 = jQuery('.price_band1').find('option:selected').data('price-band1');
+	       
+	       var akumulasiTotal = Number(priceFace1) + Number(priceFace2) + Number(priceFace3) + Number(priceBand1) + Number(priceBand2) + Number(priceBand3);
+	
+		jQuery('.akumulasi').html(akumulasiTotal);
+	});
+	
+</script>	
+
 		      <table id="qty1"><tr><td width="10%">Quantity:</td><td width="15%"><input type='text' id='qty'></td>
 			     <td><button type='submit' id='pack22cart' class="cartbnt"><img src='<?php echo $pluginDirectory;?>images/addToCart.jpg'></button></td></tr>
 			     <tr><td colspan="3" align="center"><button type='submit' onclick=' window.location.reload();' id='newWatch'><img src='<?php echo $pluginDirectory;?>images/createNew.jpg'></button></td></tr>
